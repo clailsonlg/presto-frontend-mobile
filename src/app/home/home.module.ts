89883a36a665f630b6f621bd8fa712edc82f6fc2
Clailson Lucas <clailsonlucas@gmail.com>
2021-04-11 19:22:41 -0300

    Criação dos componentes Header e Footer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 
 @NgModule({
@@ -14,6 +16,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, HeaderComponent, FooterComponent]
 })
 export class HomePageModule {}
