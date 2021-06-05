27b52f15d567a9ed4c837691dad0b1b2a6286281
Clailson Lucas <clailsonlucas@gmail.com>
2021-06-05 15:41:47 -0300

    Header and footer in app.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from 'src/app/shared/shared.module';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { StorageService } from './services/storage.service';
 import { ServicesModule } from 'src/app/services/services.module';
@@ -14,7 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, SharedModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, HttpClientModule, StorageService, NativeStorage],
   bootstrap: [AppComponent],
 })
