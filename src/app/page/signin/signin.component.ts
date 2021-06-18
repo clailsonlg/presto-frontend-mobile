edc740d0ddb46fda229c427310cdb8244f6c03fc
Clailson Lucas <clailsonlucas@gmail.com>
2021-06-17 23:40:55 -0300

    Feature signin with the id restaurant

--- src/app/page/signin/signin.component.ts
+++ src/app/page/signin/signin.component.ts
@@ -1,6 +1,8 @@
+import { RestaurantService } from './../../services/restaurant.service';
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service';
 import { UsuarioCadastro } from '../../models/usuarioCadastro';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -9,22 +11,33 @@ import { UsuarioCadastro } from '../../models/usuarioCadastro';
 })
 export class SigninComponent implements OnInit {
 
+  restaurants: String[] = [];
+
   usuario: UsuarioCadastro = {
-    dataNascimento: "",
-    email: "",
     nome: "",
-    nomeRestaurante: "Presto",
-    senha: ""
+    email: "",
+    senha: "",
+    dataNascimento: "",
+    nomeRestaurante: ""
   }
 
-  constructor(private usuarioService: UsuarioService) { }
+  constructor(private usuarioService: UsuarioService, private router: Router, private restaurantService: RestaurantService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.restaurantService.getAllRestaurants().subscribe(
+      response => {
+        response.forEach(nome => {
+          this.restaurants.push(nome);
+        })
+      }
+    )
+  }
 
   addUser() {
     this.usuarioService.addUsuario(this.usuario)
       .subscribe(response => {
         console.log(response);
+        this.router.navigate(['/login']);
       },
         error => { console.log(error) }
       )
