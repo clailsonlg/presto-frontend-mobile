c82550e3dd6e9bdd196aa4ea1f363416d999a7d1
Clailson Lucas <clailsonlucas@gmail.com>
2021-06-18 22:26:05 -0300

    Formatting home and qrcode design page, and CommonModule, FormsModuel, IonicModule imports in qr-code.module.ts

--- src/app/page/qr-code/qr-code.module.ts
+++ src/app/page/qr-code/qr-code.module.ts
@@ -1,10 +1,14 @@
 import { QrCodeRoutingModule } from './qrcode-routing.module';
 import { QrCodeComponent } from './qr-code.component';
 import { NgModule } from "@angular/core";
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
     declarations: [QrCodeComponent],
-    imports: [QrCodeRoutingModule],
+    imports: [CommonModule, FormsModule, IonicModule,QrCodeRoutingModule, SharedModule],
   })
   export class QrCodeModule {}
